1dadcd1ef06fefadb9b9ffe5909625856c734b87
Gemma Hentsch <gemma@halfapenguin.com>
2015-07-29 19:20:46 +0100

    Basic rest endpoint

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_restful import Resource, Api
 import os
 app = Flask(__name__)
+
+api = Api(app)
 
 @app.route('/')
 def hello():
 	return "Hello World! Mk2"
+
+class Listing(Resource):
+    def get(self):
+        return {"endpoints":[]}
+api.add_resource(Listing, '/listing')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
